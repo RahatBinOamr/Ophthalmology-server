ab9f5e61a8a78e7857bdef1164d5934248f363b9
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2022-11-07 23:43:58 +0600

    added DB_USER url from uri

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 /* middle ware */
 app.use(cors());
 app.use(express.json())
@@ -10,7 +11,8 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.1xniais.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.DB_URL;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
